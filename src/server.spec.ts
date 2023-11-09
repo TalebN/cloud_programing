35b693929f556563278ee1ee675bcde275553bb8
Taleb Nehme <taleb.nehme@ensta-bretagne.com>
2023-11-09 01:41:37 +0100

    push test

--- src/server.spec.ts
+++ src/server.spec.ts
@@ -3,25 +3,21 @@ import { startServer } from './server';
 describe('Test de démarrage du serveur', () => {
   let server;
 
-  beforeAll(() => {
+  it('démarre le serveur sans erreur', () => {
     server = startServer();
-  });
-
-  afterAll(async () => {
-    if (server) {
-      await new Promise((resolve) => server.close(resolve));
-    }
-  });
-
-  it('le serveur démarre sans erreur', () => {
     expect.assertions(2);
     expect(server).toBeDefined();
     expect(server.address()).toBeDefined();
   });
 
-
   it('répond à la requête /api/v1/sysinfo', async () => {
     expect.assertions(1);
     expect(true).toBe(true);
+  });
+
+  afterEach(async () => {
+    if (server) {
+      await new Promise((resolve) => server.close(resolve));
+    }
   });
 });
